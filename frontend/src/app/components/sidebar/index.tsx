6f66ea05f438d8a6fdd862725f10f3dc93cc7ee2
SamuelePassiatore <samuw.978@gmail.com>
2025-02-02 17:26:41 +0100

    created sidebar component

---
+++ frontend/src/app/components/sidebar/index.tsx
@@ -0,0 +1,34 @@
+export default function Sidebar({ selectedPage, setSelectedPage }) {
+    return (
+      <aside className="w-64 text-gray-800 flex flex-col border border-gray-300">
+        <div className="h-16 text-xl font-bold flex items-center border-b border-gray-300 ps-4">
+            <span>Gorillionaire</span>
+        </div>
+        <nav className="flex-1 p-4">
+          <ul className="space-y-2">
+            <li>
+              <button
+                className={`w-full text-left px-3 py-2 rounded-lg hover:bg-gray-200 ${
+                  selectedPage === "My Account" ? "bg-gray-200" : ""
+                }`}
+                onClick={() => setSelectedPage("My Account")}
+              >
+                My Account
+              </button>
+            </li>
+            <li>
+              <button
+                className={`w-full text-left px-3 py-2 rounded-lg hover:bg-gray-200 ${
+                  selectedPage === "Vaults" ? "bg-gray-200" : ""
+                }`}
+                onClick={() => setSelectedPage("Vaults")}
+              >
+                Vaults
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+    );
+  }
+  
