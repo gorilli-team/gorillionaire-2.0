c74715130ed2416e62a9e4d4760fa244cc8c1a2f
SamuelePassiatore <samuw.978@gmail.com>
2025-02-02 20:00:02 +0100

    add props to components

--- frontend/src/app/components/header/index.tsx
+++ frontend/src/app/components/header/index.tsx
@@ -1,30 +1,12 @@
 "use client";
+import { WalletDefault } from "@coinbase/onchainkit/wallet";
 
-import { useState } from "react";
-
-export default function Header({ selectedPage }) {
-  const [isConnected, setIsConnected] = useState(false);
-
-  const handleConnectWallet = () => {
-    setIsConnected(true);
-  };
+export default function Header() {
 
   return (
     <header className="h-16 px-6 flex items-center justify-between border-b border-gray-300 bg-gray-100">
       <div className="flex items-center justify-end space-x-4 flex-1">
-        {isConnected ? (
-          <>
-            <div className="text-sm text-gray-700">Chain</div>
-            <div className="bg-gray-200 px-3 py-1 rounded-lg text-sm text-gray-800">Wallet</div>
-          </>
-        ) : (
-          <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-400"
-            onClick={handleConnectWallet}
-          >
-            Connect Wallet
-          </button>
-        )}
+        <WalletDefault />
       </div>
     </header>
   );
--- frontend/src/app/components/main/index.tsx
+++ frontend/src/app/components/main/index.tsx
@@ -1,15 +1,18 @@
-export default function Main({ selectedPage }) {
-    const renderContent = () => {
-      switch (selectedPage) {
-        case "My Account":
-          return <div className="p-4 text-gray-800">Welcome to My Account</div>;
-        case "Vaults":
-          return <div className="p-4 text-gray-800">Here are your Vaults</div>;
-        default:
-          return <div className="p-4 text-gray-800">Select a page</div>;
-      }
-    };
-  
-    return <main className="flex-1 overflow-y-auto bg-gray-200">{renderContent()}</main>;
-  }
-  
+interface MainProps {
+  selectedPage: string;
+}
+
+export default function Main({ selectedPage }: MainProps) {
+  const renderContent = () => {
+    switch (selectedPage) {
+      case "My Account":
+        return <div className="p-4 text-gray-800">Welcome to My Account</div>;
+      case "Vaults":
+        return <div className="p-4 text-gray-800">Here are your Vaults</div>;
+      default:
+        return <div className="p-4 text-gray-800">Select a page</div>;
+    }
+  };
+
+  return <main className="flex-1 overflow-y-auto bg-gray-200">{renderContent()}</main>;
+}
