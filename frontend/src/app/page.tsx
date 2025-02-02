c8a7623e1933ebcf56d62ca1c7f03bfd3f20146f
SamuelePassiatore <samuw.978@gmail.com>
2025-02-02 17:26:57 +0100

    import components on layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+"use client";
+
+import { useState } from "react";
+import Sidebar from "./components/sidebar/index";
+import Header from "./components/header/index";
+import Main from "./components/main/index";
+
+export default function AppLayout() {
+  const [selectedPage, setSelectedPage] = useState("My Account");
+
   return (
-    <div>
-      <h1>Gorillionaire</h1>
+    <div className="flex h-screen bg-gray-100 text-gray-800">
+      <Sidebar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <div className="flex-1 flex flex-col">
+        <Header selectedPage={selectedPage} />
+        <Main selectedPage={selectedPage} />
+      </div>
     </div>
   );
 }
