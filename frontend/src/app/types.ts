f90858b9aae271664c169d7eda198be86ed05aa5
SamuelePassiatore <samuw.978@gmail.com>
2025-02-07 17:46:53 +0100

    update transaction type

--- frontend/src/app/types.ts
+++ frontend/src/app/types.ts
@@ -2,11 +2,11 @@ export interface Transaction {
     hash: string;
     method: "Deposit" | "Withdraw";
     block: number;
-    age: number;
+    age: string;
     from: string;
     amount: string;
     txnFee: string;
-}
+  }
 
 declare global {
     interface Window {
