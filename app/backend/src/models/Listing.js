16367124061a728aa735b8ca201fc4dac911f081
reekee <riccardo.galbusera@hotmail.it>
2025-03-05 17:15:57 +0100

    add listings backend management

---
+++ app/backend/src/models/Listing.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const ListingSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  tokenName: {
+    type: String,
+    required: true,
+  },
+  tokenSymbol: {
+    type: String,
+    required: true,
+  },
+  tokenDecimals: {
+    type: Number,
+    required: true,
+  },
+  tokenAddress: {
+    type: String,
+    required: true,
+  },
+  transactionHash: {
+    type: String,
+    required: true,
+  },
+  blockNumber: {
+    type: Number,
+    required: true,
+  },
+  blockTimestamp: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Listing", ListingSchema);
