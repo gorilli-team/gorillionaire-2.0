5790d812bc3789cc9c8ff485d94b33943c5b7833
Marco Roccon <marco.roccon@gmail.com>
2025-03-10 17:42:05 +0100

    Created polling script that checks for new signals every 30 minutes and push them to Nillion's Scret Vault

--- nillion/mongodb.js
+++ nillion/mongodb.js
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 import { MongoClient } from "mongodb";
@@ -9,38 +9,50 @@ let client;
 let db;
 
 export async function connectDB() {
-    if (!client) {
-        client = new MongoClient(uri);
-        await client.connect();
-        db = client.db("signals");
-        console.log("Connected to MongoDB database");
-    }
-    return db;
+  if (!client) {
+    client = new MongoClient(uri);
+    await client.connect();
+    db = client.db("signals");
+    console.log("Connected to MongoDB database");
+  }
+  return db;
 }
 
-export async function fetchData() {
-    try {
-        const db = await connectDB();
-        const generatedSignals = db.collection("generated-signals");
+export async function fetchData(currentDateISO) {
+  try {
+    const db = await connectDB();
+    const generatedSignals = db.collection("generated-signals");
 
-        const generatedSignalsDocuments = await generatedSignals.find().toArray();
+    console.log("Current date ISO:", currentDateISO);
 
-        if (generatedSignalsDocuments.length === 0) {
-            return "";
-        }
-        await closeConnection();
-        return generatedSignalsDocuments;
-    } catch (error) {
-        console.error("Error fetching data from MongoDB:", error);
-        return "";
+    const generatedSignalsDocuments = await generatedSignals.find({created_at: {$gte: new Date(currentDateISO)}}).toArray();
+
+    if (generatedSignalsDocuments.length === 0) {
+      return "";
     }
+
+    const formattedSignalData = generatedSignalsDocuments.map((doc) => {
+      let rawCreatedAt = new Date(doc.created_at);
+      let formattedCreatedAt = rawCreatedAt.toISOString();
+
+      return {
+        created_at: formattedCreatedAt,
+        signal_text: { "%allot": doc.signal_text },
+        events: { "%allot": doc.events },
+      };
+    });
+    return formattedSignalData;
+  } catch (error) {
+    console.error("Error fetching data from MongoDB:", error);
+    return "";
+  }
 }
 
 export async function closeConnection() {
-    if (client) {
-        await client.close();
-        client = null;
-        db = null;
-        console.log("MongoDB connection closed");
-    }
+  if (client) {
+    await client.close();
+    client = null;
+    db = null;
+    console.log("MongoDB connection closed");
+  }
 }
--- nillion/readWriteSv.js
+++ nillion/readWriteSv.js
@@ -4,25 +4,10 @@ dotenv.config();
 import { SecretVaultWrapper } from 'secretvaults';
 import { v4 as uuidv4 } from 'uuid';
 import { orgConfig } from './orgConfig.js';
-import { fetchData } from './mongodb.js';
 
 const SCHEMA_ID = process.env.COLLECTION_SCHEMA_ID;
 
-const rawSignalData = await fetchData();
-
-const formattedSignalData = rawSignalData.map((doc) => {
-
-    let rawCreatedAt = new Date(doc.created_at);
-    let formattedCreatedAt = rawCreatedAt.toISOString();
-
-    return ({
-        created_at: formattedCreatedAt,
-        signal_text: {'%allot': doc.signal_text},
-        events: { '%allot': doc.events}
-    })
-})
-
-async function main() {
+export async function pushNewSignalsToSecretVault(signalsToBePushed) {
     try {
       const collection = new SecretVaultWrapper(
         orgConfig.nodes,
@@ -31,24 +16,22 @@ async function main() {
       );
       await collection.init();
   
-    //   const dataWritten = await collection.writeToNodes(formattedSignalData);
-    //   console.log('dataWritten', dataWritten);
+      const dataWritten = await collection.writeToNodes(signalsToBePushed);
+      console.log('dataWritten', dataWritten);
   
-    //   const newIds = [
-    //     ...new Set(dataWritten.map((item) => item.data.created).flat()),
-    //   ];
-    //   console.log('created ids:', newIds);
+      const newIds = [
+        ...new Set(dataWritten.map((item) => item.data.created).flat()),
+      ];
+      console.log('created ids:', newIds);
   
       const dataRead = await collection.readFromNodes({});
       console.log('📚 total records:', dataRead.length);
       console.log(
         '📚 Read new records:',
-        dataRead.slice(0, formattedSignalData.length)
+        dataRead.slice(0, signalsToBePushed.length)
       );
     } catch (error) {
       console.error('❌ Failed to use SecretVaultWrapper:', error.message);
       process.exit(1);
     }
   }
-  
-  main();
