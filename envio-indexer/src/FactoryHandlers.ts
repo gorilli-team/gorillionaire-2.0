8b6066553c53f482fd5277959148acb20d5c1933
reekee <riccardo.galbusera@hotmail.it>
2025-03-31 15:14:40 +0200

    fix indexing

--- envio-indexer/src/FactoryHandlers.ts
+++ envio-indexer/src/FactoryHandlers.ts
@@ -1,9 +1,14 @@
 import { UniswapPoolFactory } from "generated";
 
-UniswapPoolFactory.PoolCreated.contractRegister(async ({ event, context }) => {
-  const token0Address = event.params.token0;
-  const token1Address = event.params.token1;
+UniswapPoolFactory.PoolCreated.contractRegister(
+  async ({ event, context }) => {
+    const token0Address = event.params.token0;
+    const token1Address = event.params.token1;
 
-  context.addToken(token0Address);
-  context.addToken(token1Address);
-});
+    context.addToken(token0Address);
+    context.addToken(token1Address);
+  },
+  {
+    preRegisterDynamicContracts: true, // Enable pre-registration for better performance
+  }
+);
