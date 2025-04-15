4aad4c457bceb82e33823cdb600250d0efcb3fa6
reekee <riccardo.galbusera@hotmail.it>
2025-04-15 13:58:50 +0200

    add nft contracts and metadata

---
+++ access-nft/script/deploy.sh
@@ -0,0 +1,7 @@
+  source .env && forge create --rpc-url https://testnet-rpc.monad.xyz \
+  --constructor-args "https://test.it" \
+  --private-key $PRIVATE_KEY \
+    src/EarlyGorillaNFT.sol:EarlyGorillaNFT \
+  --verify \
+  --verifier sourcify
+  --verifier-url 'https://sourcify-api-monad.blockvision.org' 
--- access-nft/src/AccessNFT.sol
+++ access-nft/src/AccessNFT.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract AccessNFT is ERC721, Ownable {
     error AccessNFT__InsufficientPayment();
@@ -10,10 +10,13 @@ contract AccessNFT is ERC721, Ownable {
     uint256 public s_nextTokenId;
     uint256 public s_price = 1 ether;
 
-    constructor() ERC721("Gorillionaire Access NFT", "GOR-AX") Ownable(msg.sender) {}
+    constructor()
+        ERC721("Gorillionaire Access NFT", "GOR-AX")
+        Ownable(msg.sender)
+    {}
 
     function mint() public payable {
-        if(msg.value < s_price) {
+        if (msg.value < s_price) {
             revert AccessNFT__InsufficientPayment();
         }
         uint256 tokenId = s_nextTokenId;
