ca3f200b7e750cdc7f22db999ac0ee50cbef3e7a
reekee <riccardo.galbusera@hotmail.it>
2025-04-15 14:12:18 +0200

    change base URI

--- access-nft/script/deploy.sh
+++ access-nft/script/deploy.sh
@@ -5,3 +5,5 @@
   --verify \
   --verifier sourcify
   --verifier-url 'https://sourcify-api-monad.blockvision.org' 
+
+  source .env && forge script ./script/EarlyGorillaNFT.s.sol --rpc-url https://testnet-rpc.monad.xyz --broadcast --private-key $PRIVATE_KEY
