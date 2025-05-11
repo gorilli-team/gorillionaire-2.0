a01749ba14f43002004f782efd6a5f2b6283563b
rocco <rtrombetta@gmail.com>
2025-05-11 09:41:21 +0200

    terraform state

--- terraform/aws/infrastructure/environments/development/main.tf
+++ terraform/aws/infrastructure/environments/development/main.tf
@@ -12,6 +12,14 @@ provider "aws" {
   region = var.aws_region
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "gorillionaire-development-terraform-state"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
 
 module "vpc" {
   source = "../../modules/vpc"
