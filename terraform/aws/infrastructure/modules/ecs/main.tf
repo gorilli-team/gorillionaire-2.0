24fe249272db0201a3f16738ac15e5d17bcf707d
rocco <rtrombetta@gmail.com>
2025-05-12 09:07:04 +0200

    test

--- terraform/aws/infrastructure/modules/ecs/main.tf
+++ terraform/aws/infrastructure/modules/ecs/main.tf
@@ -1,10 +1,4 @@
-resource "aws_iam_service_linked_role" "ecs" {
-  aws_service_name = "ecs.amazonaws.com"
 
-  depends_on = [
-    aws_ecs_cluster.main
-  ]
-}
 
 resource "aws_ecs_cluster" "main" {
   name = "${var.project_name}-${var.environment}-cluster"
