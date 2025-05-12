7c6ddc6b9706efe18a8b02d726c5ac4e4881f09a
rocco <rtrombetta@gmail.com>
2025-05-12 08:19:27 +0200

    add service role

--- terraform/aws/infrastructure/modules/ecs/main.tf
+++ terraform/aws/infrastructure/modules/ecs/main.tf
@@ -1,3 +1,7 @@
+resource "aws_iam_service_linked_role" "ecs" {
+  aws_service_name = "ecs.amazonaws.com"
+}
+
 resource "aws_ecs_cluster" "main" {
   name = "${var.project_name}-${var.environment}-cluster"
 
